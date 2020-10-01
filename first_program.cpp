17331e7bb0ce973dcc2e4070bbd415831812d150
Matvey <matv.com>
2020-10-01 21:44:19 +0300

    First Program

---
+++ first_program.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << ("please enter a and b for `ax + b = 0`:");
+   
+
+    int a = 0;
+    int b = 0;
+    cin >> a >> b;
+    if (a == 0)
+    {
+        cout << ("error, не дели на 0");
+    }
+    else
+    {
+        int x = -b / a;
+        cout << "solution: " << x;
+    }
+    return 0;
+}
