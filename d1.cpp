13915a6ddbcc8fef1b2d7f72e77072c702ca0fee
Dasha <dasha.terentevaa17@mail.com>
2020-10-05 23:54:08 +0300

    Initial

---
+++ d1.cpp
@@ -0,0 +1,18 @@
+﻿"Домашняя работа 1.exe" (Win32). Загружено "C:\Users\123\source\repos\Домашняя работа 1\Debug\Домашняя работа 1.exe". Символы загружены.
+"Домашняя работа 1.exe" (Win32). Загружено "C:\Windows\SysWOW64\ntdll.dll". 
+"Домашняя работа 1.exe" (Win32). Загружено "C:\Windows\SysWOW64\kernel32.dll". 
+"Домашняя работа 1.exe" (Win32). Загружено "C:\Windows\SysWOW64\KernelBase.dll". 
+"Домашняя работа 1.exe" (Win32). Загружено "C:\Windows\SysWOW64\msvcp140d.dll". 
+"Домашняя работа 1.exe" (Win32). Загружено "C:\Windows\SysWOW64\vcruntime140d.dll". 
+"Домашняя работа 1.exe" (Win32). Загружено "C:\Windows\SysWOW64\ucrtbased.dll". 
+Поток 0x42f8 завершился с кодом 0 (0x0).
+"Домашняя работа 1.exe" (Win32). Загружено "C:\Windows\SysWOW64\kernel.appcore.dll". 
+"Домашняя работа 1.exe" (Win32). Загружено "C:\Windows\SysWOW64\msvcrt.dll". 
+"Домашняя работа 1.exe" (Win32). Загружено "C:\Windows\SysWOW64\rpcrt4.dll". 
+"Домашняя работа 1.exe" (Win32). Загружено "C:\Windows\SysWOW64\sspicli.dll". 
+"Домашняя работа 1.exe" (Win32). Загружено "C:\Windows\SysWOW64\cryptbase.dll". 
+"Домашняя работа 1.exe" (Win32). Загружено "C:\Windows\SysWOW64\bcryptprimitives.dll". 
+"Домашняя работа 1.exe" (Win32). Загружено "C:\Windows\SysWOW64\sechost.dll". 
+Поток 0x14f8 завершился с кодом 0 (0x0).
+Поток 0x19d4 завершился с кодом 0 (0x0).
+Программа "[16348] Домашняя работа 1.exe" завершилась с кодом 0 (0x0).
