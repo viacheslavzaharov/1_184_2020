bd491690443ab22ffc0ea690e84f7a53bfb8780b
Rais <beats.tapping@gmail.com>
2020-10-05 23:32:52 +0300

    C++ homework

---
+++ dw.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int i = 1;
+    int j = 1;
+    do {
+        do {
+            std::cout << i << " * " << j << " = " << i * j << std::endl;
+            j++;
+        } while (j < 10);
+        i++;
+        j = 1;
+    } while (i < 10);
+    return 0;
+}
