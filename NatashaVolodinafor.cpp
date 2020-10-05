f2138367c709205f8741f2a2f7dd5acfaf93a032
Наталья Володина <70664421+natafoxy2111@users.noreply.github.com>
2020-10-05 23:52:24 +0300

    Add files via upload

---
+++ NatashaVolodinafor.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+int main()
+{
+    for (int i = 1; i <= 10; ++i)
+    {
+        for (int j = 1; j <= 10; ++j)
+            std::cout << i << " * " << j << " = " << i * j << std::endl;
+        std::cout << std::endl;
+    }
+    return 0;
+}
+
---
+++ NatashaVolodinawhile.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    int i = 1;
+    while (i < 10)
+    {
+        std::cout << i << " * " << i << " = " << i * i << std::endl;
+        i++;
+    }
+    return 0;
+}
+
+
