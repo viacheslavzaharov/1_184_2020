41a5ce59e7fe45161a20cc12e712aff2d0b6ad0e
DianaKurbanova <71451653+DianaKurbanova@users.noreply.github.com>
2020-10-06 23:00:48 +0300

    Add files via upload

---
+++ Задание 2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+int main()
+
+
+for (int i = 1; i < 10; ++i) {
+    
+for (int j = 1; j < 10; ++j) {
+       
+std::cout << i << " * " << j << " = " << i * j << std::endl;
+ 
+   }
+   
+ return 0;
+
+}
