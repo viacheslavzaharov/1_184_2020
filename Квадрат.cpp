4fbfea20c7aab86506ef90864f013abf8e51099b
DianaKurbanova <71451653+DianaKurbanova@users.noreply.github.com>
2020-10-08 23:21:07 +0300

    Квадрат
    1

---
+++ Квадрат.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+
+{
+
+int side;
+
+std::cout << "Vvedite chislo " << std::endl;
+
+std::cin >> side;
+
+for (int i = 1; i <= side; i++)
+
+{
+
+for (int j = 1; j <= side; j++)
+
+{
+
+if ( i == 1||i==side||j ==1||j==side)
+
+std::cout <<"*";
+
+
+else std::cout <<" ";
+
+}
+
+std::cout << endl;
+
+}
+
+return 0;
+
+} 
