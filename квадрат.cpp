f2751dbd04a40123573d27824048436cffa6e1e9
Valera <dmitrievvalera@example.com>
2020-10-08 14:37:48 +0300

    dmitriev

---
+++ квадрат.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+ 
+int main()
+{
+    int side;
+ 
+    std::cout << "Enter the size of the parties: ";
+    std::cin >> side;
+    std::cout << std::endl;
+ 
+    for(int i = 0; i < side; i++)
+    {
+        for(int j = 0; j < side; j++)
+        {
+            std::cout << "* ";
+        }
+        std::cout << std::endl;
+    }
+    system("pause");
+    return 0;
+}
