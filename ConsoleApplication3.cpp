b36d08ecfcddbf0ca9f1ec224d4905ce0ca2bf09
natafoxy2111 <70664421+natafoxy2111@users.noreply.github.com>
2020-10-09 01:49:26 +0300

    Homework2

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+	int size = 0;
+
+	std::cout << " Enter ";
+	std::cin >> size;
+	std::cout << std::endl;
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			std::cout << " * ";
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
+
