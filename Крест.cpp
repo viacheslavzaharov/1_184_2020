c65ae897ae7ea9e3042266da7c85450b038afebf
DianaKurbanova <71451653+DianaKurbanova@users.noreply.github.com>
2020-10-08 23:49:24 +0300

    Крест
    2

---
+++ Крест.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+
+{
+
+int side;
+
+std::cout << "Vvedite chislo " << std::endl;
+
+std::cin >> side;
+
+for (int i = 1; i <= side; i++)
+
+{
+
+for (int j = 1; j <= side; j++)
+
+{
+
+if ( i == j|| j + i ==side)
+
+std::cout <<"*";
+
+
+else std::cout <<" ";
+
+}
+std::cout << endl;
+
+}
+
+return 0;
+
+} 
