44ea28433385d2524564da0fb110cbf6d6de9fd1
artem <111@example.com>
2020-10-08 22:39:09 +0300

    3

---
+++ 1. kvadrat.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+	int k;
+	std::cout << "Введите число" << std::endl;
+	std::cin >> k;
+	{
+		for (int i = 0; i < k; i++)
+		{
+			for (int j = 0; j < k; j++)
+			{
+				std::cout << "*";
+			}
+			std::cout << std::endl;
+		}
+		return 0;
+	}
+}
