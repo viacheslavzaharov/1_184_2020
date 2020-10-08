3664cc74dd32f83870e874c25c7cb212cc383094
Valera <dmitrievvalera@example.com>
2020-10-08 23:08:01 +0300

    dmitriev

---
+++ крест.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+ 
+#include <iostream>
+ 
+int main()
+{
+int n = 0;
+std::cin >> n;
+for (int i = 1; i < n; i++) {
+for (int j = 1; j < n; j++) {
+if (i == j || i + j == n) {
+std::cout << "*";
+}
+else {
+std::cout << " ";
+}
+}
+std::cout<< std::endl;
+}
+return 0;
+}
