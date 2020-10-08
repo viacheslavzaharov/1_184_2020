6de2f628c147c80ff6d8f2b867fc65bd7f75d94d
natafoxy2111 <70664421+natafoxy2111@users.noreply.github.com>
2020-10-09 02:19:29 +0300

    Homework3

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,21 @@
+﻿//Пользователь вводит два целых числа- с длинами этих сторон на экране отрисовывается прямоугольник
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 0; 
+	int b = 0;
+	cin >> a >> b;
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < b; j++)
+		{
+			cout << " * ";
+
+		}
+		cout << endl;
+
+	}
+	return 0;
+}
