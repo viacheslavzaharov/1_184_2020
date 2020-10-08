5bfb992e221c06fc0b934c454055544565ae7765
DMITRII <presnovdmitriy02@gmail.com>
2020-10-08 23:10:37 +0400

    08/10

---
+++ дз.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	setlocale(LC_ALL, "rus");
+	cout << "Программа рисует квадрат звездочками со стороной\n";
+	cout << " Введите сторону квадрата\n";
+
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{	for (int j = 1; j <= n; j++)
+	{
+		if (i == 1 || i == n || j == 1 || j == n)
+		
+			cout << "*";
+		else cout << " ";
+	}
+	cout << endl;
+	}
+	return 0;
+}
