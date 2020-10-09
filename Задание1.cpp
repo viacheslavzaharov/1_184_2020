d5045fbd9958a72bdc89c6f5e2f0a2e53c4a2eb7
kstdnl <kostin_danila@mail.ru>
2020-10-09 14:13:46 +0300

    merge

---
+++ Задание1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+	int n = 0;
+	std::cin >> n;
+	for (int i = 1; i < n+1; i++)
+	{
+		for (int j = 1; j < n+1; j++)
+		{
+			std::cout << "*";
+		}
+
+		std::cout << std::endl;
+	}
+	return 0;
+}
---
+++ Задание2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+	int n = 0;
+	std::cin >> n;
+	for (int i = 1; i < n+1; i++)
+	{
+		for (int j = 1; j < n+1; j++)
+		{
+			if (j == i || j + i == n + 1)
+				std::cout << "*";
+			else
+				std::cout << "0";
+		}
+
+		std::cout << std::endl;
+	}
+	return 0;
+}
