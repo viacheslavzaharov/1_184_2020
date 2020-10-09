13a8179d7544c9badb775e841133fc178a3ed9c2
Uzhastin <nikitkaUzhastin@yandex.ru>
2020-10-09 21:41:05 +0300

    Homework complite

---
+++ Homework2_4.cpp
@@ -0,0 +1,15 @@
+﻿//На экране выводятся символы , перекрывая консоль ровно до того момента пока пользователь не введет любое число
+
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	while (!_kbhit()) { cout << " * "; }
+	return 0;
+}
+
+
