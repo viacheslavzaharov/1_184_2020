936db4453a4861bd4ca1b62f6ab57a66bbf28829
kstdnl <kostin_danila@mail.ru>
2020-10-09 14:11:01 +0300

    09.10.20

---
+++ finalzerocase.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+ float main()
+{
+	std::puts("please enter a and b for ax + b = 0");
+
+	float a = 0;
+	float b = 0;
+	float x = 0;
+	std::cin >> a >> b;
+	if (a != 0)
+	{
+		x = -b / a;
+		std::cout << "solution:" << x << std::endl;
+	}
+	else if (a == 0)
+	{
+		std::cout << "no solution" << std::endl;
+	}
+
+	return 0;
+
+
+}
