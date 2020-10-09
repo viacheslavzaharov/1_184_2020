8d1066484e71be47860bb90643ab816ce5c1d1fd
Uzhastin <nikitkaUzhastin@yandex.ru>
2020-10-09 17:26:29 +0300

    Class_Work1

---
+++ Classwork2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	string value = " ";
+	cout << "a: ";
+	cin >> a;
+	value = (a % 2 == 0) ? "odd" : "even";
+	cout << "Value: " << value << endl;
+
+
+
+
+
+
+}
