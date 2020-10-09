d1fa90cfc4299fc975bbe6c399b470ecb25f16f4
Uzhastin <nikitkaUzhastin@yandex.ru>
2020-10-09 09:28:29 +0300

    My Homework of 09.10.2020

---
+++ Homework2_3.cpp
@@ -0,0 +1,23 @@
+﻿// 3-Пользователь вводит два целых числа- с длинами этих сторон на экране отрисовывается прямоугольник
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	cout << "Enter two numbers, the width and height of the rectangle: ";
+	cin >> b >> a;
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < b; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+
+	}
+	return 0;
+}
