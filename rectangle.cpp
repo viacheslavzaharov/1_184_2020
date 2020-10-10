22069646519849741fd903a622383239b210c4fd
Анастасия Иванова <nastyaivanova.18.09.02@mail.ru>
2020-10-10 19:28:57 +0300

    hwm

---
+++ rectangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std; 
+int main() {
+	int a, b;
+	cout << "Enter the lenght" << endl;
+	cin >> a;
+	cout << "Enter the width" << endl;
+	cin >> b;
+	for (int i = 1; i <= b; i++)
+	{
+		for (int j = 1; j <= a; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
