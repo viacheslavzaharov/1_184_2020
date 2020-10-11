6b01759c0fb02021bd1858afb09ccf4bc386fce9
Uzhastin <nikitkaUzhastin@yandex.ru>
2020-10-11 20:56:27 +0300

    All

--- Homework2_4.cpp
+++ Homework2_4.cpp
@@ -10,88 +10,88 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "RUS");
-	char c;
+	int c=0;
 	char a;
 	char z=0;
 	while (!_kbhit()) { cout << " * "; }
-	printf("", _getch());
-	cout << "Заного нажмите на клавишу, которую вы нажимали ";
+	cout << "\n";
+
 	a = _getch();
 	if (a == '1')
 	{
-		c = 'a';
+		c = 11;
 	}
 	else if (a == '2')
 	{
-		c = 'b';
+		c = 12;
 	}
 	else if (a == '3')
 	{
-		c = 'c';
+		c = 13;
 	}
 	else if (a == '4')
 	{
-		c = 'd';
+		c = 14;
 	}
 	else if (a=='5')
 	{
-		c = 'e';
+		c = 15;
 	}
 	else if (a=='6')
 	{
-		c = 'f';
+		c = 16;
 	}
 	else if (a=='7')
 	{
-		c = 'g';
+		c = 17;
 	}
 	else if (a=='8')
 	{
-		c = 'h';
+		c = 18;
 	}
 	else if (a=='9')
 	{
-		c = 'i';
+		c = 19;
 	}
 	else if (a=='0')
 	{
-		c = 'j';
+		c = 10;
 	}
 	switch (c)
 	{
-	case 'a':
+	case 11:
 		cout << "Было введено число 1" << endl;
 		break;
-	case 'b':
+	case 12:
 		cout << "Было введено число 2" << endl;
 		break;
-	case 'c':
+	case 13:
 		cout << "Было введено число 3" << endl;
 		break;
-	case 'd':
+	case 14:
 		cout << "Было введено число 4" << endl;
 		break;
-	case 'e':
+	case 15:
 		cout << "Было введено число 5" << endl;
 		break;
-	case 'f':
+	case 16:
 		cout << "Было введено число 6" << endl;
 		break;
-	case 'g':
+	case 17:
 		cout << "Было введено число 7" << endl;
 		break;
-	case 'h':
+	case 18:
 		cout << "Было введено число 8" << endl;
 		break;
-	case 'i':
+	case 19:
 		cout << "Было введено число 9" << endl;
 		break;
-	case 'j':
+	case 10:
 		cout << " 0 - это не число, но Вы его ввели, ладно..." << endl;
 			break;
 	
 	default:
-		cout << "Буква - это не число." << endl;
+		cout << "Вы не ввели число, попробуйте заново." << endl;
 		break;
 	}
 
