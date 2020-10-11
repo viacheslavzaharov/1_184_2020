91d6de236dc53f4da677bdec847a797870bd4481
Rais <beats.tapping@gmail.com>
2020-10-12 00:45:47 +0300

    Once more push, omg

---
+++ 09_10_2020_3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	int n = 0;
+	std::cin >> a;
+	std::cin >>	b;
+	for (int i = 1; i < a; i++) {
+		for (int j = 1; j < b; j++) {
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
---
+++ discord_4.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream> 
+#include <stdio.h> 
+#include <conio.h> 
+
+using namespace std;
+
+char key;
+
+void check() {
+	key = _getch();
+
+	if (!strchr("0123456789", key)) {
+		// Crutch
+		while (!_kbhit()) {
+			cout << " * ";
+		}
+		check();
+	}
+}
+void spam() {
+	while (!_kbhit()) {
+		cout << " * ";
+	}
+	check();
+}
+
+int main()
+{
+	spam();
+
+	return 0;
+}
