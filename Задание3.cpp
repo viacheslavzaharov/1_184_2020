c40b5ca551cb68aeeb11c4b053c088dd79146d5d
kstdnl <kostin_danila@mail.ru>
2020-10-11 13:01:59 +0300

    Задания 3 и 4 на 12.10.20

---
+++ Задание3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+
+int main()
+{
+	int l = 0;
+	int w = 0;
+	std::printf("write length: ");
+	std::cin >> l;
+	std::printf("write width: ");
+	std::cin >> w;
+
+	for (int i = 1; i < l + 1; i++)
+	{
+		for (int j = 1; j < w + 1; j++)
+		{
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
---
+++ Задание4.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <conio.h>
+#include <iostream>
+#pragma warning(disable:4996)
+using namespace std;
+
+int main()
+{	
+
+	int i = 0;
+	while (1)
+	{
+		cout << i;
+		if (kbhit() != 0) break; //press any button to stop
+	}
+	return 0;
+}
