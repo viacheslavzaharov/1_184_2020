919b6e8f3fbd3cdafa318951fc5cdc6f7eb01a78
artem <111@example.com>
2020-10-12 17:37:17 +0300

    Homework 08.10

---
+++ 2. Диагонали.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+	int k;
+	std::cout << "Введите число" << std::endl;
+	std::cin >> k;
+	{
+		for (int i = 1; i < k; i++)
+		{
+			for (int j = 1; j < k; j++)
+			{
+				if (i == j || i + j == k)
+				{
+					std::cout << "0";
+				}
+				else
+				{
+					std::cout << "*";
+				}
+
+			}
+			std::cout << std::endl;
+		}
+		return 0;
+	}
+}
---
+++ 3. Прямоугольник.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+    int k, f;
+    std::cout << "Введите длину прямоугльника" << std::endl;
+    std::cin >> k;
+    std::cout << "Введите ширину прямоугльника" << std::endl;
+    std::cin >> f;
+    for (int i = 0; i < k; i++)
+    {
+        for (int j = 0; j < f; j++)
+        {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
