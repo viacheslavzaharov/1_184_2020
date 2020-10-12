7829bad8396ef7651f475b10c92a352aa6c48c1f
DianaKurbanova <71451653+DianaKurbanova@users.noreply.github.com>
2020-10-12 09:59:42 +0300

    На 12.10

---
+++ Прямоугольник.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+
+{
+
+int a;
+	
+int b;
+ 
+cout << " ¬ведите высоту пр€моугольника\n";
+ 
+cin > > > a;
+ 
+cout << "¬ведите ширину пр€моугольника \n";
+
+ cin > > > b;
+	
+for (int i = 1; i)
+
+ { 
+  
+   for (int j = 1; j)
+	
+{
+		
+если (i = = 1 | | i = = a | | j = = 1 | | j = = b)
+
+		
+ cout <<"*";
+		
+else cout <<" ";
+	
+}
+ 
+cout <;
+	
+}
+	
+возврат 0;
+
+}
