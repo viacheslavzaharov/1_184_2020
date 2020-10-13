a6cce48fe598e510631abfaa9114f6c7186adca5
Valera <dmitrievvalera@example.com>
2020-10-13 11:36:24 +0300

    dmitriev

---
+++ 1 задание (9 октября).cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 0;
+    cin >> x;
+    switch (x)
+    {
+    case 1:
+        cout << "x = 1" << "\n";
+        break;
+    case 2:
+        cout << "x = 2 " << "\n";
+        break;
+    case 3:
+        cout << "x = 3 " << "\n";
+        break;
+    default:
+        cout << "x is undefindet" << "\n";
+        break;
+    }
+    return 0;
+}
+    
+
+
---
+++ 2 задание (10 октября).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 0;
+    cin >> x;
+    switch (x)
+    {
+    case 1:
+        cout << "Понедельник" << "\n";
+        break;
+    case 2:
+        cout << "Вторник" << "\n";
+        break;
+    case 3:
+        cout << "Среда" << "\n";
+        break;
+    case 4:
+        cout << "Чертверг" << "\n";
+        break;
+    case 5:
+        cout << "Пятница" << "\n";
+        break;
+    case 6:
+        cout << "Суббота" << "\n";
+        break;
+    case 7:
+        cout << "Воскресенье" << "\n";
+        break;
+    }
+    return 0;
+}
+    
+
+
