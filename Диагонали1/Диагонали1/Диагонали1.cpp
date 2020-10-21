53472158156de99bbfef55213f26ad28decd2282
Igor <ikurbatov@mail.ru>
2020-10-21 22:16:05 +0300

    диагонали

---
+++ Диагонали1/Диагонали1/Диагонали1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int n = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j < n + 1; j++) {
+			if (i == j || i + j == n + 1 || i == 1 || j == 1 || i == n || j == n)
+				cout << "* ";
+			else
+				cout << "  ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
---
+++ Диагональ2/Диагональ2/Диагональ2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main() 
+{
+	int x = 2;
+	std::cin >> x;
+	switch (x)
+	{
+	case 1:
+		std::cout << "x = 1" << "\n";
+			break;
+	case 2:
+		std::cout << "x = 2" << "\n";
+			break;
+	case 3:
+		std::cout << "x = 1" << "\n";
+			break;
+	default:
+		std::cout << "x is undefind" << "\n";
+			break;
+	}
+}
