cd171ad4a0e4dede044b2140bbbd9783ab2c5e0e
kstdnl <kostin_danila@mail.ru>
2020-11-03 19:41:41 +0300

    лабораторная работа 5 вариант 7

---
+++ homeworkfromlab5_ variant7.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+double funct(double xmin, double xmax, double dx)
+{
+
+	for (double x = xmin; x <= xmax + dx; x += dx)
+	{
+		double y = (exp(2 * x) - exp(-x)) * sin(log(abs(x)));
+		cout << "FOR X=" << x << ", Y=" << y << endl;
+	}
+	return 0;
+}
+
+	int main()
+{
+		funct(1, 4, 0.2);
+		return 0;
+}
