420937484733073d5ba2022f26215c350b691244
kstdnl <kostin_danila@mail.ru>
2020-11-03 20:31:31 +0300

    ЛР 5 задание 2 вариант 7

---
+++ homeworkfromlab5_var7(2).cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double funct()
+{
+	double y = 1;
+	int m = 0;
+	printf("print m to count: ");
+	cin >> m;
+	for (int k = 1; k <= m; k++)
+	{
+		y = y * (exp(sin(k)));
+	}
+	cout << y;
+	return 0;
+}
+
+int main()
+{
+	funct();
+	return 0;
+}
