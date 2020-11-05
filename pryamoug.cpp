afdbb197ec8586cb9d2ac9027fa1f4f2101fafca
viacheslavzaharov <71628882+viacheslavzaharov@users.noreply.github.com>
2020-11-05 13:38:45 +0300

    Add files via upload

---
+++ pryamoug.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	cout << "Enter two numbers, the width and height of the rectangle: ";
+	cin >> b >> a;
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < b; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+
+	}
+	return 0;
+}
