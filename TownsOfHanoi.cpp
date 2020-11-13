edaac1b19dbcf9934522c55edda0558cfd5016a8
Uzhastin <nikitkaUzhastin@yandex.ru>
2020-11-14 00:01:00 +0300

    TownsOfHanoi

---
+++ TownsOfHanoi.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+void hanoi(int, int, int, int);
+
+int main()
+{
+	int n;
+	setlocale(LC_ALL, "RUS");
+	cout << "Введите n:";
+	cin >> n;
+	hanoi(n, 1, 2, 3);
+	return 0;
+}
+
+void hanoi(int n, int t1, int t2, int t3)
+{
+	if (n == 1)
+	{
+		cout << "Переместить диск по номером " << n << " на диск под номером " << t2 << endl;
+		return;
+	}
+	hanoi(n - 1, t1, t2, t3);
+	cout << "Переместить диск под номером " << n << " на диск под номером " << t2 << endl;
+	hanoi(n - 1, t3, t2, t1);
+}
+
