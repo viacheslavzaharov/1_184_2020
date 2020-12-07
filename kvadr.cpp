e8a57d02c6fd08aac598cb67492377224265c225
viacheslavzaharov <71628882+viacheslavzaharov@users.noreply.github.com>
2020-12-07 16:09:04 +0300

    works

---
+++ kvadr.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	cout << "Enter the size of the square: ";
+	cin >> a;
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < a; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+	return 0;
+
+
+
+}
